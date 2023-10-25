f60f1d57812ab139442bce7d7be420ba47017f3c
Ilya Bobrovnik <vanger_like@mail.ru>
2023-10-25 18:26:39 +0300

    Design main application

--- app/src/main/java/com/example/planapplication/MainActivity.java
+++ app/src/main/java/com/example/planapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.planapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] namesArr = new String[]{"Josh", "John", "George", "Bob", "Alex"};
+    private GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = (GridView) findViewById(R.id.gridView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.name_item, R.id.task_name, namesArr);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, (String)gridView.getItemAtPosition(i), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void goHome(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
